8ed60cd80d77628b168e7300520750e1b2b6611c
Laith Malky <laith.malky@gmail.com>
2024-12-25 20:58:29 +0200

    commit

--- day4.py
+++ day4.py
@@ -1,7 +1,17 @@
 import random
 import my_module
 
+# random number from 1 to 10
 random_integer = random.randint(1, 10)
 print(random_integer)
 
-print(my_module.MyModuleNumber)
+# how to call functions from other classes (files)
+print(my_module.MyModuleNumber) 
+
+
+# random numbers from 0.0 to 0.999 less than one 
+random_float = random.random()
+print(random_float)
+
+random_float_more_bigger_than_one = random.random() * 10
+print(random_float_more_bigger_than_one)
